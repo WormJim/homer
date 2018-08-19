29fc43e9d2de6d8dab8dbb3fd76a450cc09c7c04
Anna <anna.brakowska91@gmail.com>
2018-08-18 21:30:34 -0400

    first commit

---
+++ homer.js
@@ -0,0 +1,20 @@
+const food = {
+  1: {
+    // add level one foods
+    donut: 1,
+    guitar: 2,
+    cello: 5
+  },
+  2: {
+    beer: 10
+  },
+  3: {
+    // add level three foods
+    car: 15
+  },
+  4: {
+    // add level four foods
+    marge: 60,
+    boat: 30
+  }
+};

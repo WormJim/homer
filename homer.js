2f9b2a06f27e0771b8c664bebea235a8315444ee
davidhartman00 <davidhartman00@gmail.com>
2018-08-18 23:07:23 -0700

    building Homers head

--- homer.js
+++ homer.js
@@ -274,3 +274,54 @@ let beer4 = String.fromCharCode(
 let beer = [beer1, beer2, beer3, beer4];
 console.log('Beer');
 beer.forEach(e => console.log(e));
+
+
+let space = String.fromCharCode(32,32);
+
+let head1  = String.fromCharCode(32,32,32,32,95,95,95);
+let head2  = String.fromCharCode(32,32,32,47,47,95,92,92,95);
+let head3  = String.fromCharCode(32,46,34,92,92,32,32,32,32,34,46);
+let head4  = String.fromCharCode(47,32,32,32,32,32,32,32,32,32,32,92);
+let head5  = String.fromCharCode(124,32,32,32,32,32,32,32,32,32,32,32,92,95);
+let head6  = String.fromCharCode(124,32,32,32,32,32,32,32,44,45,45,46,45,46,41);
+let head7  = String.fromCharCode(32,92,32,32,32,32,32,47,32,32,111,32,92,111,92);
+let head8  = String.fromCharCode(32,47,92,47,92,32,32,92,32,32,32,32,47,95,47);
+let head9  = String.fromCharCode(32,32,40,95,46,32,32,32,96,45,45,39,95,95,41);
+let head10 = String.fromCharCode(32,32,32,124,32,32,32,32,32,46,45,39,32,32,92);
+let head11 = String.fromCharCode(32,32,32,124,32,32,46,45,39,32,32,32,32,32,41);
+let head12 = String.fromCharCode(32,32,32,124,32,40,32,32,95,47,46,46,45,46,39);
+let head13 = String.fromCharCode(32,32,32,124,32,32,96,46,95,95,95,46,39);
+let head14 = String.fromCharCode(32,32,32,47,32,32,32,32,40);
+let headHomer = [head1,head2,head3,head4,head5,head6,head7,head8,head9,head10,head11,head12,head13,head14]
+console.log("head")
+headHomer.forEach(e => console.log(e));
+// ___ String.fromCharCode(32,32,32,32,95,95,95)..
+// //_\\_ String.fromCharCode(32,32,32,47,47,95,92,92,95)..
+// ."\\    ". String.fromCharCode(32,46,34,92,92,32,32,32,32,34,46)...
+// /          \ String.fromCharCode(47,32,32,32,32,32,32,32,32,32,32,92)...
+// |           \_String.fromCharCode(124,32,32,32,32,32,32,32,32,32,32,32,92,95)..
+// |       ,--.-.)String.fromCharCode(124,32,32,32,32,32,32,32,44,45,45,46,45,46,41)..
+// \     /  o \o\String.fromCharCode(92,32,32,32,32,32,47,32,32,111,32,92,111,92)..
+// /\/\  \    /_/String.fromCharCode(32,47,92,47,92,32,32,92,32,32,32,32,47,95,47)..
+// (_.   `--'__)String.fromCharCode(32,32,40,95,46,32,32,32,96,45,45,39,95,95,41)..
+// |     .-'  \String.fromCharCode(32,32,32,124,32,32,32,32,32,46,45,39,32,32,92)..
+// |  .-'.     )String.fromCharCode(32,32,32,124,32,32,46,45,39,32,32,32,32,32,41)..
+// | (  _/--.-'String.fromCharCode(32,32,32,124,32,40,32,32,95,47,46,46,45,46,39)..
+// |  `.___.'String.fromCharCode(32,32,32,124,32,32,96,46,95,95,95,46,39)..
+// |    (String.fromCharCode(32,32,32,124,32,32,32,32,40)
+ // console.log(headHomer);
+
+function questionAnswer(question){
+  console.log(question);
+  
+}
+
+questionAnswer("this is the question")
+
+// this is a function that takes an array of objects. 
+// Each element of the array as an object that has a question.
+// const arrQuestion = ["What is your name",
+// "What do have any final words to say to Homer be fore he bursts",
+// ""];
+// const objAnswers = {};
+

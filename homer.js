2b09ae9cbd99ebcf9999a71dbc41341a37c80803
Grahn Cooledge <gcooled@yahoo.com>
2018-08-20 19:52:40 -0400

    cleaned code

--- homer.js
+++ homer.js
@@ -1,11 +1,3 @@
-let readline;
-let rl;
-
-function readLineFunc() {
-  readline = require('readline');
-  rl = readline.createInterface(process.stdin, process.stdout);
-}
-
 let space = String.fromCharCode(32, 32);
 let row = '';
 let str = '';
@@ -184,7 +176,6 @@ let homersHead = [
   head14,
   head15
 ];
-// console.log("head")
 
 // This function moves homer a cross the screen.
 function moveHomer(n, cb) {
@@ -209,260 +200,6 @@ function moveHomer(n, cb) {
   }
 }
 
-// // SODA
-// let straw = String.fromCharCode(32, '&#160', 32, '&#160', 32, 47, 32);
-// let top = String.fromCharCode(46, 45, 47, 45, 46);
-// let middle = String.fromCharCode(124, 39, 45, 39, 124);
-// let middle2 = String.fromCharCode(124, 42, 42, 42, 124);
-// let middle3 = String.fromCharCode(124, 42, 42, 42, 124);
-// let bottom = String.fromCharCode(92, 95, 95, 95, 47);
-
-// let soda = [straw, top, middle, middle2, middle3, bottom];
-
-// console.log('Soda');
-// soda.forEach(el => console.log(el));
-
-// // ICE CREAM
-// let topIce = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   46,
-//   45,
-//   34,
-//   96,
-//   39,
-//   34,
-//   45,
-//   46
-// );
-// let firstBall = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   47,
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   92,
-//   32
-// );
-
-// let mid = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   124,
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   124,
-//   32
-// );
-
-// let mid2 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   47,
-//   39,
-//   126,
-//   126,
-//   126,
-//   39,
-//   126,
-//   126,
-//   96,
-//   92,
-//   32
-// );
-
-// let mid3 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   124,
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   124,
-//   32
-// );
-
-// let mid4 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   92,
-//   95,
-//   46,
-//   45,
-//   45,
-//   46,
-//   95,
-//   46,
-//   45,
-//   46,
-//   95,
-//   47
-// );
-
-// let cone1 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   92,
-//   61,
-//   95,
-//   61,
-//   95,
-//   61,
-//   95,
-//   47
-// );
-// let cone2 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   92,
-//   61,
-//   95,
-//   61,
-//   95,
-//   47
-// );
-
-// let cone3 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   92,
-//   61,
-//   95,
-//   47
-// );
-// let cone4 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   92,
-//   47
-// );
-
-// let iceCream = [
-//   topIce,
-//   firstBall,
-//   mid,
-//   mid2,
-//   mid3,
-//   mid4,
-//   cone1,
-//   cone2,
-//   cone3,
-//   cone4
-// ];
-
-// console.log('Icecream');
-// iceCream.forEach(e => console.log(e));
-
-// let beer1 = String.fromCharCode(32, '&#160', 95, 111, 79, 111, 79, 111);
-// let beer2 = String.fromCharCode(91, 95, 124, 124, 124, 124, 124);
-// let beer3 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   124,
-//   124,
-//   124,
-//   124,
-//   124
-// );
-// let beer4 = String.fromCharCode(
-//   32,
-//   '&#160',
-//   32,
-//   '&#160',
-//   126,
-//   126,
-//   126,
-//   126,
-//   126
-// );
-// let beer = [beer1, beer2, beer3, beer4];
-// console.log('Beer');
-// beer.forEach(e => console.log(e));
 let consoleBrowser = ''; //flag to know where we are operating, 'B' for Browser, 'T' for terminal
 
 const foodLevels = [
@@ -508,109 +245,6 @@ const foodLevels = [
     santas_little_helper: 80
   }
 ];
-// **** TERMINAL FUNCTIONS **** //
-function introT() {
-  const string =
-    '\nWelcome to Happy, Happy Homer.\n' +
-    "A fun game Homer from the Simpsons can't stop eating\n" +
-    'Feed him everything you can to satisfy his craving.\n';
-  console.log(string);
-}
-
-function gameRulesT(player) {
-  console.log(`${player}, let's go over the game play`);
-  let gamePlayString =
-    'The objective is to feed Homer a variety of items.\n' +
-    "If he doesn't explode, you advance to the next level.\n" +
-    'Beware though, Homer might randomly explode\n' +
-    "Try not to feed him too fast or you'll be coverd in everything he ate.";
-
-  console.log(gamePlayString);
-}
-
-function setHomerT(lvl) {
-  console.log(
-    `Level ${lvl}\nHomer has ${lvl *
-      100} points\nDrop that down to zero to advance to the next level.`
-  );
-  return lvl * 100;
-}
-
-// **** ^ TERMINAL FUNCTIONS **** //
-function gameOver() {}
-
-function levelUpTerminal(lvl) {
-  console.log('In Level Up');
-  return lvl++;
-}
-
-function setHomerTerminal() {
-  let lvl = 1;
-  let items = foodLevels[lvl - 0];
-  return () => {
-    return lvl * 100;
-  };
-}
-
-function homerMustEat(answer, food, bellySize, levelFood) {
-  if (answer.toLowerCase().trim() === 'exit') {
-    rl.close();
-  }
-
-  console.log(food);
-  console.log(food.hasOwnProperty(answer));
-
-  if (!levelFood.hasOwnProperty(answer)) {
-    rl.setPrompt(`\nImproper argument, please choose again:\n${levelFood} > `);
-    rl.prompt();
-  } else {
-    bellySize -= food[lvl - 1][answer];
-    if (bellySize < 25) {
-      rl.setPrompt(
-        `\nHomer is almost full, choose something else:\n ${levelFood} > `
-      );
-      rl.prompt();
-    } else {
-      rl.setPrompt(
-        `\nHomer is still hungry, see what else he can eat:\n${levelFood} > `
-      );
-      rl.prompt();
-    }
-  }
-}
-
-function feedHomerTerminal(food, lvl) {
-  let bellySize = setHomerTerminal(lvl);
-  let levelFood = Object.keys(food[lvl - 1]);
-
-  rl.question(`\nFeed Homer:\n ${levelFood}? > `, answer => {
-    homerMustEat(answer, food, bellySize, levelFood);
-    rl.on('line', answer => {
-      homerMustEat(answer, food, bellySize, levelFood);
-      // bellySize -= food[lvl - 1][answer];
-
-      // if (bellySize <= 0) {
-      //   rl.close();
-      // } else if (bellySize <= 25) {
-      //   rl.setPrompt(
-      //     `Homer is almost full, choose something else and hope he doesn't explode:\n ${levelFood} > `
-      //   );
-      //   rl.prompt();
-      // } else {
-      //   rl.prompt();
-      // }
-    });
-
-    // rl.on('close', () => {
-    //   console.log('Closed');
-    //   feedHomerTerminal(food, levelUpTerminal(lvl));
-    // });
-  });
-}
-
-// #########################
-// ## BROWESER CODE BELOW ##
-// #########################
 
 function introB() {
   const string =
@@ -659,23 +293,18 @@ function feedHomer(food, lvl) {
   return feedHomer(foodLevels, lvl + 1);
 }
 
-
-// ***** dual version functions ***** //
 function initGame() {
-  if (typeof window === 'undefined') {
-    consoleBrowser = 'T';
-    introT();
-    readLineFunc();
-    let homer = setHomerTerminal();
-    // feedHomerTerminal(foodLevels, 1);
-    feedHomerTerminal(homer());
-  } else {
-    consoleBrowser = 'B';
-    // intro();
-    let player = setPlayer();
-    gameRulesB(player);
-    feedHomer(foodLevels, 1);
+  console.log('browser');
+  if (intro()) {
+    if (setPlayer()) {
+      if (gameRules()) {
+      }
+    }
   }
+  // intro();
+
+  // gameRulesB(player);
+  // feedHomer(foodLevels, 1);
 }
 
 moveHomer(125, initGame);

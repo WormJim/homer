df44fd590fd3b02d1078bb8e508c9f00acd8ecc9
davidhartman00 <davidhartman00@gmail.com>
2018-08-20 13:07:11 -0700

    second commit, rewound to the beginning when the Exit choice works

--- test.js
+++ test.js
@@ -1,35 +1,26 @@
 // ver 1
 function inOut(qst,cb,oth){
-  console.log(qst, "qst line 3"); //sned the question to terminal
-  let stdin = process.openStdin(); //invoke the Nodejs object process.openStdin to detect and collect user input
+  console.log(qst);
+  let stdin = process.openStdin();
 
-   function startTerminal(){ //since process.operStdin is asyinc we need to force JS to wait for a response.
-      let termAns =[]
-      stdin.addListener("data", function(d) {
-          let resAns = d.toString().trim()
-          console.log("line 10", resAns)
-          if(resAns.toLowerCase() === 'exit'){
-            console.log(
-              'Your leaving us already? OK.\nJust as Homer would say,\n'
-              + "Kids, just because I don't care doesn't mean I'm not listening."
-            )
-            process.stdin.pause();
-            termAns = ['exit', false]
-            return termAns
-          }else{ 
-             
-             while (termAns[0] === 'undefined'){
-                termAns = [resAns,true]
-             }
-            return termAns
-          }
-      })
-    }  
+  stdin.addListener("data", function(d) {
+      let resAns = d.toString().trim()
+      if(resAns.toLowerCase() === 'exit'){
+        console.log(
+          'Your leaving us already? OK.\nJust as Homer would say,\n'
+          + "Kids, just because I don't care doesn't mean I'm not listening."
+        )
+        process.stdin.pause();
+      }else{ 
+        return resAns
+      }
+    })
+      
 }
       
 (function testStart(){
   let myAns = inOut("this question");
-  console.log(myAns, "myAns, line 32")
+  console.log(myAns)
 })()
 // console.log("enter anything");
 // var stdin = process.openStdin();
